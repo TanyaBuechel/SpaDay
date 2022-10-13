d6ec3acf0ce04623817432709b15cf4dd147e353
Tanya Buechel <tanyabuechel@gmail.com>
2022-10-12 21:58:31 -0500

    Having issues with verifying password

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.Name = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                return Redirect("/User/Add");
+            }
+        }
+    }
+}
