1ff78e3c86a7e9ed73cfc192921c6c8384ab0c73
Tanya Buechel <tanyabuechel@gmail.com>
2022-10-19 17:49:18 -0500

    Needs a little fixing

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -5,29 +5,46 @@ namespace SpaDay.Controllers
 {
     public class UserController : Controller
     {
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("/user/")]
         public IActionResult Index()
         {
             return View();
         }
 
+        // GET: /<controller>/add
         [HttpGet]
+        [Route("/user/add")]
         public IActionResult Add()
         {
             return View();
         }
 
-        
+        // POST: /<controller>/
+        [HttpPost]
+        [Route("/user/")]
         public IActionResult SubmitAddUserForm(User newUser, string verify)
         {
-            if (newUser.Password == verify)
+            ViewBag.username = newUser.Username;
+            ViewBag.password = newUser.Password;
+            if (ViewBag.password == verify)
             {
-                ViewBag.Name = newUser.Username;
                 return View("Index");
             }
             else
             {
-                return Redirect("/User/Add");
+                ViewBag.error = true;
+                return Redirect("/user/add");
             }
+            
+            //if (ViewBag.password != verify)
+            //{
+            //    ViewBag.error = true;
+            //    return Redirect("/user/add");
+           // }
+           // return View("Index");
         }
+        
     }
 }
