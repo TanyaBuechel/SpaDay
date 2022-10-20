0ec118b8ac5002ce679b4a54e134eec5fe085ce8
Tanya Buechel <tanyabuechel@gmail.com>
2022-10-19 20:56:18 -0500

    FInished studio

---
+++ SpaDay/ViewModel/AddUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModel
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage ="Username must be between 5 to 15 characters.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 to 20 characters.")]
+        public string Password { get; set; }
+
+        [EmailAddress(ErrorMessage ="Must be an email!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password verification is required.")]
+        public string VerifyPassword { get; set; }
+    }
+}
